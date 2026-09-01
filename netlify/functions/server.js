fe23c267fc52ba65a1e57a04a4646f36bc920100
Ervan Jovian <ervanjovian@gmail.com>
2026-09-01 19:33:47 +0700

    Back to vercel

--- netlify/functions/server.js
+++
@@ -1,5 +0,0 @@
-const { createRequestHandler } = require('@expo/server/adapter/netlify');
-
-module.exports = createRequestHandler({
-  build: require('path').join(__dirname, '../../dist/server'),
-});
